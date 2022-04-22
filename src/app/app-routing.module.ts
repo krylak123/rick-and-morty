5ccf0476422ed9e4593cab0a5707f1970f0223d4
Krystian Grzelak <krystian.grzelak@envelo.pl>
2022-04-22 09:11:33 +0200

    feast: set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':seasonNumber',
+    component: DetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
